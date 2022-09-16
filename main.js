f850066dd8d60a81b547caa72789d7976258c6af
Dngtint-Loptin <jimindngtint@gmail.com>
2022-09-16 23:03:29 +0700

    done lesson 1+2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//xu ly slider
+//Xử lý slide
 
 var slides = document.getElementsByClassName("slide-item");
 var dots = document.getElementsByClassName("btn-around");
@@ -30,30 +30,27 @@ function showSlides(n) {
 
 showSlides(slideIndex);
 
-//click nut sang phai
+//click nút sang phải
 document.querySelector(".btn-right").addEventListener("click",()=>{
     slideIndex +=1 ;
     showSlides(slideIndex);
 })
 
-//click nut sang trai
+//click nút sang trái
 document.querySelector(".btn-left").addEventListener("click",()=>{
     slideIndex -=1 ;
     showSlides(slideIndex);
 })
 
-//click nut tron
+//Click nút chấm chuyển slide
 for(let i = 0; i< dots.length ; i++){
     dots[i].addEventListener("click",()=>{
         slideIndex = i;
         showSlides(slideIndex);
-        console.log(dots[i]);
-        console.log(i);
     })
 }
 
-
-//tu dong chuyen slide 
+//Tự động chuyển slide 5 giây 
 var auto = setInterval(function(){
     slideIndex+=1;
     showSlides(slideIndex);
@@ -62,4 +59,93 @@ var auto = setInterval(function(){
 
 
 
-// Xu ly form 
+
+//Xử lý form
+
+//Đối tượng validator
+function Validator(options){
+    var formElement = document.querySelector(options.form); //lấy phần tử form
+
+    if(formElement){
+        options.rules.forEach((rule)=>{
+            var inputElement = formElement.querySelector(rule.selector); // lấy phần tử input trong form
+            var errorElement= inputElement.parentElement.querySelector(".form-message"); //lấy ra phần tử message
+            var submit = document.querySelector('.form-submit');//nút submit
+
+            if(inputElement){
+                //Xử lý trường hợp Blur khỏi input: khi blur khỏi input sẽ kiểm tra và thông báo lỗi (nếu có)
+                inputElement.onblur = ()=>{
+                    var errorMessage = rule.test(inputElement.value); //Cho giá trị nhập vào hàm test để kiểm tra
+
+                    //Có lỗi
+                    if(errorMessage){
+                        errorElement.innerText = errorMessage;
+                        inputElement.parentElement.classList.add("active");
+                        submit.classList.add('disable');
+                    }
+                    else{
+                        errorElement.innerText = "";
+                        inputElement.parentElement.classList.remove("active");
+                        submit.classList.remove('disable');
+                    }
+                }
+
+                //Xử lý mỗi khi người dùng nhập: Khi đang nhập nếu đang hiện lỗi sẽ ẩn lỗi đi
+                inputElement.oninput= ()=>{
+                    errorElement.innerText = "";
+                    inputElement.parentElement.classList.remove("active");
+                    submit.classList.add('disable');
+                }
+            }
+
+        })
+    }
+    
+}
+
+//Định nghĩa các rule
+//vai trò : Khi có lỗi thì trả ra message lỗi
+Validator.isRequired = (selector)=>{
+    return{
+        selector: selector,
+        test: (value)=>{
+            return value.trim() ? undefined : "Vui lòng nhập trường này"; //thông báo lỗi
+        }
+    }
+}
+
+Validator.isEmail = (selector)=>{
+    return{
+        selector: selector,
+        test: (value)=>{
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+            return regex.test(value)? undefined : "Trường này phải là email"; //thông báo lỗi
+        }
+    }
+}
+
+Validator.isPassword = (selector,min,max)=>{
+    return{
+        selector: selector,
+        test: (value)=>{
+            var message = "";
+            var partten = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/ ;
+            if(value.length < min||value.length > max){
+                message +="Mật khẩu nằm trong khoảng từ 8-32 ký tự\n";
+            }
+            if(!partten.test(value)){
+                message += "Mật khẩu phải có cả chữ hoa, chữ thường và số\n"
+            }
+            return message.length<=1? undefined:message ;
+        }
+    }
+}
+
+Validator.isConfirm = (selector, getConfirmValue)=>{
+    return{
+        selector: selector,
+        test: (value)=>{
+            return value == getConfirmValue()? undefined: "Mật khẩu không chính xác";
+        }
+    }
+}
